57bce5c746db81ba15d6c47b6d966ba92fd6b6ad
baptistebisson <bbisson.baptiste@gmail.com>
2018-08-03 15:15:06 +0200

    Fix include

--- app/Helpers/Utils.php
+++ app/Helpers/Utils.php
@@ -152,4 +152,27 @@ class Utils
         return $response->get();
     }
 
+    public function toArray($data) {
+        return json_decode(json_encode($data), true);
+    }
+
+    /**
+     * Make string ready to use as a filename
+     * @param string $str
+     * @return null|string|string[]
+     */
+    public function normalizeString ($str = '') {
+        $str = strip_tags($str);
+        $str = preg_replace('/[\r\n\t ]+/', ' ', $str);
+        $str = preg_replace('/[\"\*\/\:\<\>\?\'\|]+/', ' ', $str);
+        $str = strtolower($str);
+        $str = html_entity_decode( $str, ENT_QUOTES, "utf-8" );
+        $str = htmlentities($str, ENT_QUOTES, "utf-8");
+        $str = preg_replace("/(&)([a-z])([a-z]+;)/i", '$2', $str);
+        $str = str_replace(' ', '-', $str);
+        $str = rawurlencode($str);
+        $str = str_replace('%', '-', $str);
+        return $str;
+    }
+
 }
